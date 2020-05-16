4811a027918d01199b26a57944928c3a5d902db8
rakinar2 <63922752+rakinar2@users.noreply.github.com>
2020-05-16 15:39:56 +0600

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,56 @@
 
+
+/*!
+*   Thread Javascript Library
+*   Coded & Developed by Ar. Rakin
+*   GitHub: 
+*     - https://rakinar2.github.io/
+*     - https://github.com/rakinar2/
+*   Copyright (c) Ar. Rakin 2020,
+*   All Rights Reserved.
+*   NOTE: This Javascript Library is in development.
+*         More features will be added here soon. (Like JQuery)
+*/
+
+
+// Selector function
+
+var $ = function(selector){
+    element = document.querySelector(selector);
+    return element;
+};
+
+// useful methods to '$' object
+
+  $.html = function(element){
+    return $(element).innerHTML;
+  };
+  
+  $.text = function(element){
+    return $(element).innerTEXT;
+  };
+    
+  $.fadeOut = function(element, speed){
+      var a = setInterval(fn, 100);
+      var c = $(element);
+      var s = c.style.opacity;
+      
+      function fn() {
+       c.style.transition = speed + "s";
+       c.style.opacity = s - "0";
+      }
+      
+  };
+  
+  $.fadeIn = function(element, speed){
+      var a = setInterval(fn, 100);
+      var c = $(element);
+      var s = c.style.opacity;
+      
+      function fn() {
+       c.style.transition = speed + "s";
+       c.style.opacity = s + "1";
+      }
+      
+  };
+
